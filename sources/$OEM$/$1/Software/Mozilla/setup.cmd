0c58095058111d363ca729a841d28ea986b6c0f8
Sion <sion@email.com>
2020-07-13 14:23:41 +0000

    Move Mozilla relevant settings into one folder

--- sources/$OEM$/$1/Software/Mozilla/setup.cmd
+++ sources/$OEM$/$1/Software/Mozilla/setup.cmd
@@ -2,6 +2,8 @@
 @echo off
 pushd %~dp0
 
+for %%i in (*.reg) do reg import "%%i"
+
 for /d %%i in (*) do (
 	for /f "tokens=3*" %%j in ('reg query "HKLM\Software\Mozilla\Mozilla %%~ni" /v "Install Directory" /s ^| find "REG_SZ"') do (
 		robocopy "%%i" "%%k" /s
