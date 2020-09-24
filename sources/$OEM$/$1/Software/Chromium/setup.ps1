78dcc60ad639ddf5471dc8839ef53dc052d30e87
Sion <sion@email.com>
2020-09-24 15:22:32 +0000

    Improve how to import Chromium based browsers group policies

--- sources/$OEM$/$1/Software/Chromium/setup.ps1
+++ sources/$OEM$/$1/Software/Chromium/setup.ps1
@@ -6,10 +6,8 @@ $User_Data = Get-ChildItem -Filter 'User Data' -Recurse -Name -Directory
 Get-ChildItem $User_Data -Recurse -Name -File | Select-Object -Unique | ForEach-Object {$chromium = Join-Path 'Chromium\User Data' $_; Join-Path $User_Data $_ -PipelineVariable browser | ForEach-Object {Join-Path $Config $_ | ForEach-Object {-join (Get-Content ($_, $chromium, $browser | Get-Unique) -Raw -ErrorAction 0) -replace '\s*}\s*{', ',' | Set-Content $_ -NoNewline}}}
 
 
-reg import 'Chromium.reg'
-$key = Select-String -Pattern '^\[(.+)\]$' -Path *.reg -List | ForEach-Object {$_.Matches.Groups[1].Value}
-$chromium = $key.Where({$_.EndsWith('\Chromium')})
-$key.Where({-not $_.EndsWith('\Chromium')}).ForEach({reg copy $chromium $_ /s /f})
-Get-Item '*.reg' | ForEach-Object {reg import $_}
+$Keys = Select-String -Pattern '(?<=^\[)[^]]+(?=\]$)' -Path '*.reg' -List | Group-Object {$_.Matches.Value.EndsWith('\Chromium')}
+$Chromium = $Keys[0].Group.ForEach({reg import $_.Path; $_.Matches.Value})
+$Keys[1].Group.ForEach({reg copy $Chromium[0] $_.Matches.Value /s /f; reg import $_.Path})
 
 Pop-Location
