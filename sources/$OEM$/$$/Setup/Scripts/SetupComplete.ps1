5d3e271401df5b3cf2c92598591c4e792e5388cb
Sion <sion@email.com>
2020-11-11 15:31:14 +0000

    Use ExpandString value kind for expanding environment variable in registry

--- sources/$OEM$/$$/Setup/Scripts/SetupComplete.ps1
+++ sources/$OEM$/$$/Setup/Scripts/SetupComplete.ps1
@@ -35,7 +35,7 @@ w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.o
 (Get-Item 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Run').Property.ForEach({[Microsoft.Win32.Registry]::SetValue('HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run', $_, [byte[]]3)})
 
 # Download and install apps when any network connection is available
-[Microsoft.Win32.Registry]::SetValue('HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce', 'Install', 'schtasks /create /tn Install /xml "%SystemDrive%\Software\Install.xml" /f')
+[Microsoft.Win32.Registry]::SetValue('HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce', 'Install', 'schtasks /create /tn Install /xml "%SystemDrive%\Software\Install.xml" /f', [Microsoft.Win32.RegistryValueKind]::ExpandString)
 
 # Configure system and apps for the system account
 Join-Path $env:SystemDrive 'Software\Config\Registry\System\*.reg' -Resolve | ForEach-Object {reg import $_}
@@ -47,7 +47,7 @@ reg load 'HKU\Default' (Join-Path $env:SystemDrive 'Users\Default\NTUSER.DAT')
 (Get-Item 'HKEY_USERS\Default\Software\Microsoft\Windows\CurrentVersion\Run').Property.ForEach({[Microsoft.Win32.Registry]::SetValue('HKEY_USERS\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run', $_, [byte[]]3)})
 
 # Configure system and apps for a new user account when signing in to the computer for the first time
-[Microsoft.Win32.Registry]::SetValue('HKEY_USERS\Default\Software\Microsoft\Windows\CurrentVersion\RunOnce', 'Setup', '%SystemDrive%\Software\Config\setup.cmd')
+[Microsoft.Win32.Registry]::SetValue('HKEY_USERS\Default\Software\Microsoft\Windows\CurrentVersion\RunOnce', 'Setup', 'PowerShell -ExecutionPolicy Bypass -File "%SystemDrive%\Software\Config\setup.ps1"', [Microsoft.Win32.RegistryValueKind]::ExpandString)
 
 reg unload 'HKU\Default'
 
