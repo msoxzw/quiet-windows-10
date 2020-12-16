842e60581a25e67e39b411bd18604a3ccaffa8c2
Sion <sion@email.com>
2020-12-16 09:37:30 +0000

    Simplify Chromium preferences setup logic

--- sources/$OEM$/$1/Software/Chromium/setup.ps1
+++ sources/$OEM$/$1/Software/Chromium/setup.ps1
@@ -3,7 +3,7 @@ Push-Location $PSScriptRoot
 $Config = '..\Config\Files\LocalAppData', $env:LocalAppData
 $Config.ForEach({Copy-Item (Get-ChildItem -Directory) $_ -Force -Filter PSIsContainer -Recurse})
 $User_Data = Get-ChildItem -Filter 'User Data' -Recurse -Name -Directory
-Get-ChildItem $User_Data -Recurse -Name -File | Select-Object -Unique | ForEach-Object {$chromium = Join-Path 'Chromium\User Data' $_; Join-Path $User_Data $_ -PipelineVariable browser | ForEach-Object {Join-Path $Config $_ | ForEach-Object {-join (Get-Content ($_, $chromium, $browser | Get-Unique) -Raw -ErrorAction 0) -replace '\s*}\s*{', ',' | Set-Content $_ -NoNewline}}}
+Get-ChildItem 'Chromium\User Data' -Recurse -Name -File | ForEach-Object {$chromium = Join-Path 'Chromium\User Data' $_; $browser = Join-Path $User_Data $_; $browser | ForEach-Object {Join-Path $Config $_ | ForEach-Object {-join (Get-Content ($_, $chromium, $browser | Get-Unique) -Raw -ErrorAction 0) -replace '\s*}\s*{', ',' | Set-Content $_ -NoNewline}}}
 
 
 $Keys = Select-String -Pattern '(?<=^\[)[^]]+(?=\]$)' -Path '*.reg' -List | Group-Object {$_.Matches.Value.EndsWith('\Chromium')}
