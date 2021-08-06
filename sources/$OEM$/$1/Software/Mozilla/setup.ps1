e9cc7e02ab21bd48a8e4f291618c7297618a643b
Sion <sion@email.com>
2021-08-06 03:22:10 +0000

    A better method of removing "Default Browser Agent"

--- sources/$OEM$/$1/Software/Mozilla/setup.ps1
+++ sources/$OEM$/$1/Software/Mozilla/setup.ps1
@@ -4,4 +4,6 @@ Resolve-Path '*.reg' | ForEach-Object {reg import $_}
 
 Get-ChildItem -Directory | ForEach-Object {$Config = Join-Path $_ '*'; Get-ItemPropertyValue "HKLM:\Software\Mozilla\*$_*\*\Main" 'Install Directory' | ForEach-Object {Copy-Item $Config $_ -Force -Recurse}}
 
+Get-Item 'HKCU:\Software\Mozilla\*\Default Browser Agent' | ForEach-Object {$agent = Join-Path ($_.Property[0] -split '\|')[0] 'default-browser-agent'; Get-ChildItem (Join-Path $_.PSParentPath 'Installer') -Name | ForEach-Object {& $agent uninstall $_}}
+
 Pop-Location
