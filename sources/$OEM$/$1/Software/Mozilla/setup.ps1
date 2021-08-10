4c1f6df7a0e978f1e85517f177759a3400578eb2
Sion <sion@email.com>
2021-08-10 14:07:02 +0000

    Detect Mozilla install directory from both HKCU and HKLM

--- sources/$OEM$/$1/Software/Mozilla/setup.ps1
+++ sources/$OEM$/$1/Software/Mozilla/setup.ps1
@@ -2,7 +2,7 @@ Push-Location $PSScriptRoot
 
 Resolve-Path '*.reg' | ForEach-Object {reg import $_}
 
-Get-ChildItem -Directory | ForEach-Object {$Config = Join-Path $_ '*'; Get-ItemPropertyValue "HKLM:\Software\Mozilla\*$_*\*\Main" 'Install Directory' | ForEach-Object {Copy-Item $Config $_ -Force -Recurse}}
+Get-ChildItem -Directory | ForEach-Object {$Config = Join-Path $_ '*'; Get-ItemPropertyValue (Join-Path 'HKCU:', 'HKLM:' "Software\Mozilla\*$_*\*\Main") 'Install Directory' | ForEach-Object {Copy-Item $Config $_ -Force -Recurse}}
 
 Get-Item 'HKCU:\Software\Mozilla\*\Default Browser Agent' | ForEach-Object {$agent = Join-Path ($_.Property[0] -split '\|')[0] 'default-browser-agent'; Get-ChildItem (Join-Path $_.PSParentPath 'Installer') -Name | ForEach-Object {& $agent uninstall $_}}
 
