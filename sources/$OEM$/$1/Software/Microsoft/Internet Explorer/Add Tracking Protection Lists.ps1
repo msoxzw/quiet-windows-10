a21fc25622bca0f053e2c2c3bab33eeab1b5c175
Sion <sion@email.com>
2021-08-17 09:43:12 +0000

    Prefer main adblockpluscore repo to mirror of it

--- sources/$OEM$/$1/Software/Microsoft/Internet Explorer/Add Tracking Protection Lists.ps1
+++ sources/$OEM$/$1/Software/Microsoft/Internet Explorer/Add Tracking Protection Lists.ps1
@@ -1,13 +1,13 @@
 Push-Location $PSScriptRoot
 
-Start-BitsTransfer 'https://raw.githubusercontent.com/adblockplus/adblockpluscore/master/data/subscriptions.json'
+Start-BitsTransfer 'https://gitlab.com/eyeo/adblockplus/abc/adblockpluscore/-/raw/master/data/subscriptions.json' -Dynamic
 $subscriptions = Get-Content '*.json' -Raw | ConvertFrom-Json | ForEach-Object {$_}
 
 $language = (Get-UICulture).TwoLetterISOLanguageName
 if ($language -notin $subscriptions.languages) {$language = 'en'}
 
 $registryPath = 'HKCU:\Software\Microsoft\Internet Explorer\Safety\PrivacIE\Lists'
-$added = Get-ItemPropertyValue "$registryPath\*" 'Name'
-$subscriptions | Where-Object {$_.title -notin $added -and ($null -eq $_.languages -or $language -in $_.languages)} | ForEach-Object {[PSCustomObject]@{'Enabled' = 1; 'Name' = $_.title; 'Url' = $_.url -replace '\.txt$','.tpl'} | Set-ItemProperty (New-Item  $registryPath -Name "{$(New-Guid)}".ToUpper() -Force).PSPath}
+$added = Get-ItemPropertyValue "$registryPath\*" 'Name' -ErrorAction 0
+$subscriptions | Where-Object {$_.title -notin $added -and ($null -eq $_.languages -or $language -in $_.languages)} | ForEach-Object {[PSCustomObject]@{Enabled = 1; Name = $_.title; Url = $_.url -replace '\.txt$','.tpl'} | Set-ItemProperty (New-Item  $registryPath -Name "{$(New-Guid)}".ToUpper() -Force).PSPath}
 
 Pop-Location
