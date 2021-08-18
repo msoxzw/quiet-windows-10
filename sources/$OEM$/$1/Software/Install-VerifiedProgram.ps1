8f1b40a8be0b907c7f2886825458d6549b0cadda
Sion <sion@email.com>
2021-08-18 14:28:08 +0000

    Convert function to simple script

--- sources/$OEM$/$1/Software/Install-VerifiedProgram.ps1
+++ sources/$OEM$/$1/Software/Install-VerifiedProgram.ps1
@@ -1,35 +1,30 @@
 
-function Install-VerifiedProgram {
+param (
+    [Parameter(Mandatory, Position=0)]
+    [uri]$Source,
 
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory, Position=0)]
-        [uri]$Source,
+    [Parameter(Position=1)]
+    [string]$Arguments,
 
-        [Parameter(Position=1)]
-        [string]$Arguments,
+    [string]$Destination = (Split-Path $Source -Leaf),
 
-        [string]$Destination = (Split-Path $Source -Leaf),
+    [switch]$Dynamic,
 
-        [switch]$Dynamic,
+    [int]$RetryInterval = 600
+)
 
-        [int]$RetryInterval = 600
-    )
-
-    while ($true) {
-        Start-BitsTransfer $Source $Destination -Dynamic:$Dynamic
-        if ($?) {
-            $Signature = Get-AuthenticodeSignature $Destination
-            if ($Signature.Status -eq 'Valid') {
-                return [System.Diagnostics.Process]::Start(@{FileName = $Signature.Path; Arguments = $Arguments; Verb = 'runas'})
-            }
-            Write-Warning $Signature.StatusMessage
+while ($true) {
+    Start-BitsTransfer $Source $Destination -Dynamic:$Dynamic
+    if ($?) {
+        $Signature = Get-AuthenticodeSignature $Destination
+        if ($Signature.Status -eq 'Valid') {
+            return [System.Diagnostics.Process]::Start(@{FileName = $Signature.Path; Arguments = $Arguments; Verb = 'runas'})
         }
-        $Date = (Get-Date).AddSeconds($RetryInterval)
-        for ($i = $RetryInterval; -not $Host.UI.RawUI.KeyAvailable -and $i -gt 0; $i--) {
-            Write-Progress "Retry after $Date, press a key to continue ..." 'Waiting' -SecondsRemaining $i
-            Start-Sleep 1
-        }
+        Write-Warning $Signature.StatusMessage
     }
-
+    $Date = (Get-Date).AddSeconds($RetryInterval)
+    for ($i = $RetryInterval; -not $Host.UI.RawUI.KeyAvailable -and $i -gt 0; $i--) {
+        Write-Progress "Retry after $Date, press a key to continue ..." 'Waiting' -SecondsRemaining $i
+        Start-Sleep 1
+    }
 }
--- sources/$OEM$/$1/Software/Microsoft/Office/setup.ps1
+++ sources/$OEM$/$1/Software/Microsoft/Office/setup.ps1
@@ -1,8 +1,6 @@
 Push-Location $PSScriptRoot
 
-. '..\..\Install-VerifiedProgram.ps1'
-
 # https://docs.microsoft.com/deployoffice/overview-office-deployment-tool
-$null = Install-VerifiedProgram 'https://officecdn.microsoft.com/pr/wsus/setup.exe' '/configure'
+& '..\..\Install-VerifiedProgram.ps1' 'https://officecdn.microsoft.com/pr/wsus/setup.exe' '/configure'
 
 Pop-Location
