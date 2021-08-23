7ad5fd543de3ec604e3bc5e68796bee33597540a
Sion <sion@email.com>
2021-08-23 15:08:18 +0000

    Direct pass scriptblock used to generate download uri

--- sources/$OEM$/$1/Software/Adobe/Reader/Install.ps1
+++ sources/$OEM$/$1/Software/Adobe/Reader/Install.ps1
@@ -1,12 +1,14 @@
 Push-Location $PSScriptRoot
 
 if (Get-Package 'Adobe Acrobat *') {exit}
-$reader = (Invoke-WebRequest 'https://get.adobe.com/reader/webservices/json/standalone/?language=Hindi' -UseBasicParsing -Headers @{'X-Requested-With' = 'XMLHttpRequest'} | ConvertFrom-Json) | Group-Object {$_.queryName.EndsWith('(64Bit)')} -AsHashTable
 
-$url = ($reader.[Environment]::Is64BitOperatingSystem).download_url
-if ($url -eq $null) {$url = $reader.Values.download_url}
+$ScriptBlock = {
+    $reader = (Invoke-WebRequest 'https://get.adobe.com/reader/webservices/json/standalone/?language=Hindi' -UseBasicParsing -Headers @{'X-Requested-With' = 'XMLHttpRequest'} | ConvertFrom-Json) | Group-Object {$_.queryName.EndsWith('(64Bit)')} -AsHashTable
+    $download_url = ($reader.[Environment]::Is64BitOperatingSystem).download_url
+    if ($download_url) {$download_url} else {$reader.Values.download_url}
+}
+$Arguments = if ($args) {$args} else {'/sALL'}
 
-if (-not $args) {$args = '/sALL'}
-& '..\..\Install-VerifiedProgram.ps1' $url $args
+& '..\..\Install-VerifiedProgram.ps1' $ScriptBlock $Arguments
 
 Pop-Location
