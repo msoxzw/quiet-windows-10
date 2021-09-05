5563be02a8f52e6d04b2d66f2967848a3032185d
Sion <sion@email.com>
2021-09-05 14:06:30 +0000

    Make Thunderbird preferences more consistent with Firefox

--- sources/$OEM$/$1/Software/Mozilla/Thunderbird/thunderbird.js
+++ sources/$OEM$/$1/Software/Mozilla/Thunderbird/thunderbird.js
@@ -1,6 +1,8 @@
 // Mozilla Preferences
 
 defaultPref("browser.cache.disk.enable", false);
+defaultPref("browser.cache.memory.capacity", getPref("browser.cache.disk.capacity"));
+defaultPref("browser.cache.memory.max_entry_size", getPref("browser.cache.disk.max_entry_size"));
 defaultPref("browser.formfill.enable", false);
 defaultPref("browser.region.network.url", "");
 clearPref("browser.search.region");
@@ -17,6 +19,7 @@ defaultPref("mail.winsearch.firstRunDone", true);
 defaultPref("mailnews.start_page.enabled", false);
 pref("mailnews.start_page_override.mstone", "ignore");
 defaultPref("media.peerconnection.ice.relay_only", true);
+defaultPref("network.captive-portal-service.enabled", false);
 defaultPref("network.connectivity-service.enabled", false);
 defaultPref("network.cookie.lifetimePolicy", 2);
 defaultPref("places.history.enabled", false);
