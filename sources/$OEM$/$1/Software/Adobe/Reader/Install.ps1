6296870a7644141b69c42549c91189341fd9b0fb
Sion <sion@email.com>
2021-09-16 15:01:16 +0000

    Prefer Adobe Readers with specific language to MUI MUI vesion of Reader currently does not offer 64 Bit version, and has ugly language hack "Hindi"

--- sources/$OEM$/$1/Software/Adobe/Reader/Install.ps1
+++ sources/$OEM$/$1/Software/Adobe/Reader/Install.ps1
@@ -9,9 +9,8 @@ Push-Location $PSScriptRoot
 if (Get-Package 'Adobe Acrobat *') {exit}
 
 $ScriptBlock = {
-    $reader = (Invoke-WebRequest 'https://get.adobe.com/reader/webservices/json/standalone/?language=Hindi' -UseBasicParsing -Headers @{'X-Requested-With' = 'XMLHttpRequest'} | ConvertFrom-Json) | Group-Object {$_.queryName.EndsWith('(64Bit)')} -AsHashTable
-    $download_url = ($reader.[Environment]::Is64BitOperatingSystem).download_url
-    if ($download_url) {$download_url} else {$reader.Values.download_url}
+    $reader = (Invoke-WebRequest 'https://get.adobe.com/reader/webservices/json/standalone/' -UseBasicParsing -Headers @{'Accept-Language' = (Get-UICulture).Name; 'X-Requested-With' = 'XMLHttpRequest'} | ConvertFrom-Json) | Group-Object {$_.queryName.EndsWith('(64Bit)')} -AsHashTable
+    ($reader.[Environment]::Is64BitOperatingSystem).download_url
 }
 
 & '..\..\Install-VerifiedProgram.ps1' $ScriptBlock $Arguments
