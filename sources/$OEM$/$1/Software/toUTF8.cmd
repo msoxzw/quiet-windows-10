4058e0e6c64e0f69f65845f09906cd95bf20b484
Sion <sion@email.com>
2021-09-19 11:33:32 +0000

    Support `toUTF8.cmd` for PowerShell prior to v6

--- sources/$OEM$/$1/Software/toUTF8.cmd
+++ sources/$OEM$/$1/Software/toUTF8.cmd
@@ -1,5 +1,6 @@
 @echo off
 
-set "command=& {$args | ForEach-Object {(Get-Content -LiteralPath $_ -Raw) -replace "`r`n", "`n" | Set-Content -LiteralPath $_ -NoNewline}} %*"
+REM https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_character_encoding
+set "command=& {$args | ForEach-Object {(Get-Content -LiteralPath $_ -Raw) -replace "`r`n", "`n" | New-Item $_ -Force}} %*"
 
 PowerShell "%command:"=\"%"
