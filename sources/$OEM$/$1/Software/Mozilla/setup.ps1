d473eb4c9bdb9fa93791713d561315d8bffc51c8
Sion <sion@email.com>
2021-09-19 12:05:01 +0000

    Slightly improve Mozilla setup script

--- sources/$OEM$/$1/Software/Mozilla/setup.ps1
+++ sources/$OEM$/$1/Software/Mozilla/setup.ps1
@@ -1,7 +1,7 @@
 Push-Location $PSScriptRoot
 
 $Products = Import-PowerShellDataFile 'Products.psd1'
-Get-ChildItem -Name -Directory | ForEach-Object {$Configuration = Join-Path $_ '*'; $Products.$_.Values.Name | ForEach-Object {Get-Package "$_ (*)" -ErrorAction 0} | ForEach-Object {Copy-Item $Configuration $_.Metadata['InstallLocation'] -Force -Recurse}}
+Get-ChildItem -Name -Directory | ForEach-Object {$Configuration = Join-Path $_ '*'; Get-Package $Products.$_.Values.Name.ForEach({"$_ (*)"}) -ErrorAction 0 | ForEach-Object {Copy-Item $Configuration $_.Metadata['InstallLocation'] -Force -Recurse}}
 
 (Get-ScheduledTask '* Default Browser Agent *').Actions | ForEach-Object {& $_.Execute uninstall $_.Arguments.Split()[1]}
 
