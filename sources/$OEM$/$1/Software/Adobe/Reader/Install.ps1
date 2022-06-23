648b9fbd2a8082dbe1e1f5d4e457f60fc35fc05f
Sion <sion@email.com>
2022-06-23 14:01:39 +0000

    Adobe Acrobat Reader download API has altered

--- sources/$OEM$/$1/Software/Adobe/Reader/Install.ps1
+++ sources/$OEM$/$1/Software/Adobe/Reader/Install.ps1
@@ -17,9 +17,20 @@ process
     if (Get-Package 'Adobe Acrobat *') {return}
 
     $ScriptBlock = {
-        $reader = (Invoke-WebRequest 'https://get.adobe.com/reader/webservices/json/standalone/' -UseBasicParsing -Headers @{'Accept-Language' = (Get-UICulture).Name; 'X-Requested-With' = 'XMLHttpRequest'} | ConvertFrom-Json) | Group-Object {$_.queryName.EndsWith('(64Bit)')} -AsHashTable
-        ($reader.[Environment]::Is64BitOperatingSystem).download_url
+        $uri = 'https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/'
+        $uri += (Invoke-WebRequest $uri -UseBasicParsing).Links.Where({$_.href.StartsWith('continuous/')}, 'Default', 1).href
+        $uri = (Invoke-WebRequest $uri -UseBasicParsing).Links.Where({$_.href.EndsWith('_MUI.msp')}, 'Default', 2).href
+        Start-BitsTransfer $uri[[Environment]::Is64BitOperatingSystem] 'patch.msp'
+
+        if ([Environment]::Is64BitOperatingSystem) {
+            'https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/2100120135/AcroRdrDCx642100120135_MUI.exe'
+        }
+        else {
+            'https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_MUI.exe'
+        }
     }
+
+    $Arguments += '/msi PATCH="{0}"' -f (Join-Path $PSScriptRoot 'patch.msp')
 
     $ExitCode = Install-VerifiedProgram $ScriptBlock $Arguments -RetryInterval $RetryInterval
 }
